1e52552abde95afba31a401c03fa134c1247a07c
Vedx23 <paturkarvedant@gmail.com>
2024-04-10 19:41:17 +0530

    feat: accept connection to the socket

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,58 @@
-#include<iostream>
-#include<sys/types.h>
-#include<unistd.h>
-#include<netdb.h>
-#include<arpa/inet.h>
-#include<string.h>
-#include<string>
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
 
 int main(int argc, char const *argv[])
 {
     // 1. create  socket
     int listening = socket(AF_INET, SOCK_STREAM, 0);
-    if(listening == -1) {
-        std::cerr<<"error creating a socket"<<std::endl;
+    if (listening == -1)
+    {
+        std::cerr << "error creating a socket" << std::endl;
         return -1;
     }
 
     // 2. give some ip and port to the socket
-    sockaddr_in hint; 
-    hint.sin_family = AF_INET; //set the family to IPV4
-    hint.sin_port = htons(54000); //host to nextwork short 
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);//convert an ip address to a string
-    //hint.sin_addr is a buffer
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;                     // set the family to IPV4
+    hint.sin_port = htons(54000);                  // host to nextwork short
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // convert an ip address to a string
+    // hint.sin_addr is a buffer
 
-    if( bind(listening,reinterpret_cast<sockaddr*>(&hint), sizeof(hint))){
-        std::cerr<<"cannot bind to IP/port"<<std::endl;
+    if (bind(listening, reinterpret_cast<sockaddr *>(&hint), sizeof(hint)))
+    {
+        std::cerr << "cannot bind to IP/port" << std::endl;
         return -2;
     }
     // 3. start listening in on the socket
+    if (listen(listening, SOMAXCONN) == -1)
+    {
+        std::cerr << "error while trying to listen" << std::endl;
+        return -3;
+    }
     // 4. accept a connection on the socket
+    sockaddr_in client;
+    socklen_t clientsize = sizeof(client);
+    char host(NI_MAXHOST);
+    char svc(NI_MAXSERV);
+
+    int clientsocket = accept(listening, reinterpret_cast<sockaddr *>(&client), reinterpret_cast<socklen_t *>(&clientsize));
+
+    if (clientsocket == -1)
+    {
+        std::cerr << "err connecting to client socket\n";
+        return -4;
+    }
+
     // 5. close the socket for listening
+    
     // 6. display the receiving message
     // 7. close the socket
-    
-    std::cout<<"hello world"<<std::endl;
+
+    std::cout << "hello world" << std::endl;
     return 0;
 }
