7a365d9c19cf55ebde42f32fd6c607eacb563a6b
Vedx23 <paturkarvedant@gmail.com>
2024-04-10 15:35:49 +0530

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    /*
+    1. create  socket
+    2. give some ip and port to the socket
+    3. start listening in on the socket
+    4. accept a connection on the socket
+    5. close the socket for listening
+    6. display the receiving message
+    7. close the socket
+    */
+    return 0;
+}
